16bdb24f90a42948ad55a98871778c410f32df63
16084 <16084@my.sanctamaria.school.nz>
2022-03-07 18:24:50 +1300

    flask blog - lesson 14
    added login.html page
    added route to login on main_app

--- main_app.py
+++ main_app.py
@@ -53,13 +53,19 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for("home"))
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-# route for login page
-@app.route("/login")
+# Route for login page 
+@app.route("/login", methods=['GET', 'POST']) 
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else: 
+            flash('Login unsuccessful. Please check your username and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 # allows main app to run the web server
