f88e60570d6fcdcd76d09cce8e2f53a092ba35a8
16084 <16084@my.sanctamaria.school.nz>
2022-03-13 20:39:50 +1300

    flask blog - lesson 18 (lesson 20)
    created package
    
    changed file setup for program to run as a package

---
+++ main_app/__init__.py
@@ -0,0 +1,10 @@
+# blog app that allows users to register and post blogs 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a469bb9b2a9e4dabd2b03e1fa2c816e8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+from main_app import routes
