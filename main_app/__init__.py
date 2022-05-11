585020e850ea672a2d6287606220a8c71944fbb0
16084 <16084@my.sanctamaria.school.nz>
2022-05-11 12:17:03 +1200

    lesson 21 (flask blog 19)
    Download Bcrypt and embedded it unto code

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -1,10 +1,12 @@
 # blog app that allows users to register and post blogs 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a469bb9b2a9e4dabd2b03e1fa2c816e8'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from main_app import routes
