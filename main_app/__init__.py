2aebcb2d68784b283b08cb6e086343d84c49b6d7
16084 <16084@my.sanctamaria.school.nz>
2022-05-23 09:51:41 +1200

    lesson 23 - flask blog 21
    added current_user
    added logout route
    imported usermixin

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -2,11 +2,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a469bb9b2a9e4dabd2b03e1fa2c816e8'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from main_app import routes
